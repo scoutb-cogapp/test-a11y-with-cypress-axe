0a6025de323496745e80499bcd9c69c77103c05a
scoutb-cogapp <scoutb@cogapp.com>
2024-04-26 11:39:22 +0100

    corrected path to save AA reports

--- cypress/e2e/a11y.cy.js
+++ cypress/e2e/a11y.cy.js
@@ -1,9 +1,9 @@
-describe("Different levels a11y testing", () => {
+describe("Test accessibility for different WCAG levels", () => {
   const now = new Date();
   const page =
     "https://peck.ackland.org/artworks/figures-dancing-around-a-fire";
 
-  it("should run accessibility audits for WCAG A and AA", () => {
+  it("should run accessibility audits for WCAG 2.2 A and AA", () => {
     cy.visit(page);
     cy.injectAxe();
     cy.checkA11y(
@@ -11,16 +11,16 @@ describe("Different levels a11y testing", () => {
       {
         runOnly: {
           type: "tag",
-          values: ["wcag2a", "wcag2aa"],
+          values: ["wcag22a", "wcag22aa"],
         },
       },
       (violations) => {
-        cy.writeFile("../a11y-reports/a11y-AA.json", violations, "utf8");
+        cy.writeFile("./cypress/a11y-reports/a11y-AA.json", violations, "utf8");
       }
     );
   });
 
-  it("should run accessibility audits for WCAG AAA", () => {
+  it("should run accessibility audits for WCAG 2.2 AAA", () => {
     cy.visit(page);
     cy.injectAxe();
     cy.checkA11y(
@@ -28,7 +28,7 @@ describe("Different levels a11y testing", () => {
       {
         runOnly: {
           type: "tag",
-          values: ["wcag2aaa"],
+          values: ["wcag22aaa"],
         },
       },
       (violations) => {
